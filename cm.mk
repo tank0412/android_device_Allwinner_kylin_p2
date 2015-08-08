e0c1bac866eba3551da10b3bf9843ca97c2eb80a
Roman <romabukhtiarov@gmail.com>
2015-08-08 14:10:22 +0300

    cm.mk & full_v989.mk files were updated

--- cm.mk
+++ cm.mk
@@ -1,8 +1,14 @@
 $(call inherit-product, device/onda/v989/full_v989.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_v989
 DEVICE_RESOLUTION := 1536x2048
 TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
+
+PRODUCT_NAME := cm_v989
+PRODUCT_DEVICE := v989
+PRODUCT_BRAND := Onda
+PRODUCT_MODEL := ONDA_V989
+PRODUCT_MANUFACTURER := onda
 
 
